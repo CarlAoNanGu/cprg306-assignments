d8894e5459d4a682c2e0692c553a59a7134b97ad
carlnangu <>
2024-09-12 14:11:25 -0600

    Added Week-2 Title

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -4,6 +4,7 @@ import StudentInfo from "./student-info";
 export default function Page() {
     return (
         <main>
+            <h3>Week 2</h3>
             <StudentInfo />
         </main>
     );
