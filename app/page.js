7c9ebdd8ca71df3ea9503dbb82398b273e7c1277
carlnangu <>
2024-09-12 13:14:21 -0600

    Finished week-2 Assignment
    -Finished page.js in week-2
    -Updated main page.js with week-2 link and title
    -Added GitHub Link to week-2 page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+          <Link href="./week-2" className="underline text-cyan-600 hover:text-cyan-300" >Week 2</Link>
+        </li>
+      </ul>
+    </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h3>Ao Nan (Carl) Gu</h3>
+            <Link href="https://github.com/CarlAoNanGu/cprg306-assignments" className="underline text-cyan-600 hover:text-cyan-300">My GitHub Repository</Link>
+        </div>
+    )
+}
