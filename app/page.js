2727f1a19578fefaf85723b907e602812c704431
carlnangu <>
2024-09-19 13:00:35 -0600

    Sep 19 update
    changed some headers and began week 3 project

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
 
+let linkStyles = "underline text-cyan-600 hover:text-cyan-300"
+
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
-        <li>
-          <Link href="./week-2" className="underline text-cyan-600 hover:text-cyan-300" >Week 2</Link>
-        </li>
+        <li><Link href="./week-2" className={linkStyles} >Week 2 - Assignment</Link></li>
+        <li><Link href="./week-3/objects" className={linkStyles} >Week 3 - Shopping List</Link></li>
       </ul>
     </main>
   );
