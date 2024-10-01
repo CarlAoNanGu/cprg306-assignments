04d321579f7ed8671426cdb505d11338e7f53295
carlnangu <>
2024-10-01 14:37:20 -0600

    Centered Shopping List

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -4,9 +4,9 @@ export default function Item({ name, quantity, category }) {
 
     return (
         <div className="bg-blue-100">
-            <h3 className="font-bold ">{name}</h3>
-            <p className="">Buy {quantity}</p>
-            <p className="">in {category}</p>
+            <h3 className="font-bold text-center">{name}</h3>
+            <p className="text-center">Buy {quantity}</p>
+            <p className="text-center">in {category}</p>
         </div>
     )
 }
